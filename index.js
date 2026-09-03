7b7061fb9fd03e27b6c240921a65589910e53bb3
Blitz-sys <nuuke32blitz@gmail.com>
2026-09-03 17:08:21 +0530

    Remove non-essential form attributes and update JavaScript validation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const form = document.querySelector("form");
 const checkbox = form.querySelector("input[type='checkbox']");
 const submit = form.querySelector("button[type='submit']");
+const Name = form.querySelector("[name='name']");
+const email = form.querySelector("[name='email']");
+const experience = form.querySelector("[name='experience']");
+Name.placeholder = "Enter Name";
+email.placeholder = "Enter Email";
+experience.placeholder = "Describe your experience so far";
 form.addEventListener("input", (event) => {
   event.preventDefault();
-  const name = form.querySelector("input[type='text']").value;
-  const email = form.querySelector("input[type='email']").value;
-  const experience = document.getElementById("exp").value;
-  if (name && email && experience && checkbox.checked) {
+  if (Name.value && email.validity.valid && experience.value && checkbox.checked) {
     submit.disabled = false;
   }
   else {
